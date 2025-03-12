690ab87263b28659c6d3bf95bf0441e00b5b8909
lilzdotgovph <108351739+lilzdotgovph@users.noreply.github.com>
2025-03-12 16:17:27 +0800

    Update kpmg2.js

--- kpmg2.js
+++ kpmg2.js
@@ -1,5 +1,6 @@
 function def(){
- document.getElementById('MainBody').innerHTML = "U R HACKED!";
+ document.getElementById('MainBody').innerHTML = "THE PAGE HAS BEEN MODIFIED!";
+ document.write("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Logo_of_KPMG.svg/1200px-Logo_of_KPMG.svg.png'>");
 }
 
 def();
