d9a51e2b45005effc0501dd0c79bbf3a7b2763bc
lilzdotgovph <108351739+lilzdotgovph@users.noreply.github.com>
2025-03-12 17:02:20 +0800

    Update kpmg2.js

--- kpmg2.js
+++ kpmg2.js
@@ -1,6 +1,7 @@
 function def(){
  document.getElementById('MainBody').innerHTML = "THE PAGE HAS BEEN MODIFIED!";
- document.write("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Logo_of_KPMG.svg/1200px-Logo_of_KPMG.svg.png'>");
+ document.getElementById('MainBody').style
+ document.write("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Logo_of_KPMG.svg/1200px-Logo_of_KPMG.svg.png' width='100px' height='100px'>");
 }
 
 def();
